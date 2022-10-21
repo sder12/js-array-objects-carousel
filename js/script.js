96b0f35682d4dbe30ee7275739e2f69c25f28fe3
sder12 <sara.cetto@gmail.com>
2022-10-21 19:22:23 +0200

    Create 3 array, using the index - infinite cicle

--- js/script.js
+++ js/script.js
@@ -2,26 +2,9 @@
 //[*] prelevare i chevron dall'html
 //[*]prelevare il contenitore dell'img :slider-img
 //[*]prelevare il contenitore del txt :slider-text
-//[*] aggiungere l eventListener hai due btn 
-    //[] ripulire div
-    //[] togliere la classe visible al div dell'img
-    //[] togliere la classe visible al div del testo
-    //[] INSERIRE UN'IMG
-        //[] entrare nell'array con ciclo for 
-        //[] entrare nel object with []
-        //[] estrapolare dall'obj il link dell'img 
-        //[] inserire il link dell'img nel DOM
-        //[] aggiungere la classe visible al slider-img
-        //[] appendere l'img al div slider-img
-    //[] INSERIRE IL TESTO
-        //[] entrare nell'array con ciclo for
-        //[] entrare in un elemento con ciclo for in 
-        //[] estrapolare dall'obj il contenuto del TITLE
-        //[] inserire il link nel slider-text dentro un <h2>
-        //[] estrapolare dall'obj il contenuto del TEXT
-        //[] inserire il link nel slider-text dentro un <p>
-        //[] aggiungere la classe visible al slider-text
-        //[] appedere i due elementi creati al slider-text
+//[*] aggiungere l eventListener hai due btn
+//[*] ripulire div
+
 
 //TAKE from the DOM
 const leftBtn = document.getElementById("chevron-left");
@@ -29,34 +12,72 @@ const rigthBtn = document.getElementById("chevron-rigth");
 const sliderImg = document.querySelector(".slider-img");
 const sliderText = document.querySelector(".slider-text");
 
+//CREO 3 ARRAY : 1 IMG 1 TITLE 1 TEXT
+const arrayImages = [];
+const arrayTitle = [];
+const arrayText = [];
+images.forEach((item) => {
+  const imageLink = item["image"];
+  const titleLink = item["title"];
+  const textLink = item["text"];
+  arrayImages.push(imageLink);
+  arrayTitle.push(titleLink);
+  arrayText.push(textLink);
+});
+console.log(arrayImages);
+console.log(arrayTitle);
+console.log(arrayText);
+
+//USO L'INDEX delle ARRAY per placeholder
+let sliderPosition = 0;
+creationElementAddDom();
 
 //RIGHT BTN
-rigthBtn.addEventListener("click", function(){
-    //cilo for in the array
-    images.forEach((item) => {
-        // console.log(item);  
-        const imageLink = item["image"];      
-        const titleLink = item["title"];
-        const textLink = item["text"];
-
-        console.log(imageLink)
-        console.log(titleLink) 
-        console.log(textLink)
-        console.log("----") 
-    });
+rigthBtn.addEventListener("click", function () {
+  cleanHtml();
+  //   alert("right btn");
+  if (sliderPosition >= 4) {
+    sliderPosition = 0;
+  } else {
+    sliderPosition++;
+  }
+  creationElementAddDom();
 });
 
 //LEFT BTN
-leftBtn.addEventListener("click", function(){
-    alert("left btn");
+leftBtn.addEventListener("click", function () {
+  cleanHtml();
+  if (sliderPosition > 0) {
+    sliderPosition--;
+  } else {
+    sliderPosition = 4;
+  }
+  creationElementAddDom();
 });
 
 
 
 
+//FUNCTION UI 
+/**
+ * Description: Clean the all the slider
+ */
+function cleanHtml() {
+  sliderImg.innerHTML = "";
+  sliderText.innerHTML = "";
+}
 
-
-
-
-
-
+/**
+ * Description Create the elements and add them in the Dom 
+ * the IMG, the TITLE and the PARAGRAPH
+ */
+function creationElementAddDom() {
+  //CREATION ELEMENTS HTML
+  textImages = `<img src="${arrayImages[sliderPosition]}" alt="">`;
+  textTitle = `<h2>${arrayTitle[sliderPosition]}</h2>`;
+  textText = `<p>${arrayText[sliderPosition]}</p>`;
+  //INSERISCO NEL DOM
+  sliderImg.innerHTML += textImages;
+  sliderText.innerHTML += textTitle;
+  sliderText.innerHTML += textText;
+}
