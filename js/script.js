8c560e5eec30f1648db8d026d60d390a94569adc
sder12 <sara.cetto@gmail.com>
2022-10-21 17:07:02 +0200

    Add chevrons' eventListener + ciclo for - array

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 //[*] prelevare i chevron dall'html
 //[*]prelevare il contenitore dell'img :slider-img
 //[*]prelevare il contenitore del txt :slider-text
-//[] aggiungere l eventListener hai due btn 
+//[*] aggiungere l eventListener hai due btn 
     //[] ripulire div
     //[] togliere la classe visible al div dell'img
     //[] togliere la classe visible al div del testo
     //[] INSERIRE UN'IMG
-        //[] entrare nell'array con ciclo for
-        //[] entrare in un elemento con ciclo for in 
+        //[] entrare nell'array con ciclo for 
+        //[] entrare nel object with []
         //[] estrapolare dall'obj il link dell'img 
         //[] inserire il link dell'img nel DOM
         //[] aggiungere la classe visible al slider-img
@@ -23,11 +23,32 @@
         //[] aggiungere la classe visible al slider-text
         //[] appedere i due elementi creati al slider-text
 
+//TAKE from the DOM
 const leftBtn = document.getElementById("chevron-left");
 const rigthBtn = document.getElementById("chevron-rigth");
-console.log(leftBtn, rigthBtn);
 const sliderImg = document.querySelector(".slider-img");
 const sliderText = document.querySelector(".slider-text");
-console.log(sliderImg);
-console.log(sliderText);
+
+
+//RIGHT BTN
+rigthBtn.addEventListener("click", function(){
+alert("right btn");
+});
+
+//LEFT BTN
+leftBtn.addEventListener("click", function(){
+    alert("left btn");
+});
+
+
+
+
+images.forEach((item) => {
+    // console.log(item);  
+        console.log(item["image"]);
+        console.log(item["title"])
+        console.log(item["text"])   
+        console.log("----") 
+});
+
 
