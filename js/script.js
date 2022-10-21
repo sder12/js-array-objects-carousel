beee8e5d72b74a850c6a595551f7509d84440fb2
sder12 <sara.cetto@gmail.com>
2022-10-21 16:52:25 +0200

    Js pseudocode

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// PSEUDOCODICE
+//[] prelevare i chevron dall'html
+//[]prelevare il contenitore dell'img :slider-img
+//[]prelevare il contenitore del txt :slider-text
+//[] aggiungere l eventListener hai due btn 
+    //[] ripulire div
+    //[] togliere la classe visible al div dell'img
+    //[] togliere la classe visible al div del testo
+    //[] INSERIRE UN'IMG
+        //[] entrare nell'array con ciclo for
+        //[] entrare in un elemento con ciclo for in 
+        //[] estrapolare dall'obj il link dell'img 
+        //[] inserire il link dell'img nel DOM
+        //[] aggiungere la classe visible al slider-img
+        //[] appendere l'img al div slider-img
+    //[] INSERIRE IL TESTO
+        //[] entrare nell'array con ciclo for
+        //[] entrare in un elemento con ciclo for in 
+        //[] estrapolare dall'obj il contenuto del TITLE
+        //[] inserire il link nel slider-text dentro un <h2>
+        //[] estrapolare dall'obj il contenuto del TEXT
+        //[] inserire il link nel slider-text dentro un <p>
+        //[] aggiungere la classe visible al slider-text
+        //[] appedere i due elementi creati al slider-text
+
